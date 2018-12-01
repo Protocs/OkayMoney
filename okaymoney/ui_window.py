935420090c50647aa4c61fbf8de60716f3ea7ee3
Protocs <utki.nikita@yandex.ru>
2018-12-02 00:11:51 +0500

    Добавил главный файл, класс UIWindow и шаблон класса LoginWindow

---
+++ okaymoney/ui_window.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import uic
+
+
+class UIWindow(QMainWindow):
+    """Класс окна, который автоматически подгружает UI из файла по пути, содержащемся в переменной класса ``ui_path``.
+
+    Пример::
+
+        class LoginWindow(UIWindow):
+            ui_path = 'ui/login.ui'
+            ...
+    """
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui/login.ui')
