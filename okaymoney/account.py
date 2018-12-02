1ed508fc7b0fbe654751f7f83d6a2ea827a5ea30
Protocs <utki.nikita@yandex.ru>
2018-12-02 14:41:20 +0500

    Переместил Account.save(), Account.load() в модуль user_save_load, добавил Account.add_transaction()

--- okaymoney/account.py
+++ okaymoney/account.py
@@ -11,21 +11,7 @@ class Account:
         self.balance = balance
         self.SAVE_PATH = name + '.okm'
 
-    def save(self, path):
-        """Сохраняет себя в файл по пути ``f``."""
-        try:
-            with open(path, 'w') as f:
-                pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
-        except OSError as e:
-            messagebox.error(f'Невозможно создать файл для сохранения профиля: {path}\n({e})')
-
-    @staticmethod
-    def load(path):
-        """Загружает профиль из файла по пути ``f`` и возвращает его."""
-        try:
-            with open(path) as f:
-                return pickle.load(f)
-        except OSError as e:
-            messagebox.error(f'Невозможно открыть файл профиля: {path}\n({e})')
-        except pickle.PickleError as e:
-            messagebox.error(f'Ошибка загрузки профиля: {path}\n({e})')
+    def add_transaction(self, tr):
+        """Добавляет транзакцию в историю и изменяет баланс."""
+        self._transactions.append(tr)
+        self.money += tr.delta
