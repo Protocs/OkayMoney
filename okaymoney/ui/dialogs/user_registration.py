fadf69616350fda7933ae3083a0c09f88c7a8839
Protocs <utki.nikita@yandex.ru>
2018-12-02 21:24:30 +0500

    Добавил шаблон диалога регистрации пользователя

---
+++ okaymoney/ui/dialogs/user_registration.py
@@ -0,0 +1,12 @@
+from .ui_dialog import UIDialog
+
+
+class TransactionAddDialog(UIDialog):
+    """Диалог регистрации нового пользователя.
+
+    *Файл интерфейса:* ``ui/dialogs/user_registration.ui``
+    """
+
+    ui_path = 'ui/dialogs/user_registration.ui'
+
+    ...
