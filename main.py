14f57483bfd0ea5902a2006323cb839ba4dad6be
Protocs <utki.nikita@yandex.ru>
2018-12-02 19:04:41 +0500

    Поставил стиль Fusion

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QStyleFactory
 
 from okaymoney.ui.login import LoginWindow
 
 # Класс окна, появляющегося при запуске программы.
 FIRST_WINDOW = LoginWindow
 
+# Стиль приложения
+STYLE = 'Fusion'
+
 app = QApplication(sys.argv)
+
+st = QStyleFactory.create(STYLE)
+app.setStyle(st)
+
 w = FIRST_WINDOW()
 w.show()
 exit(app.exec())
