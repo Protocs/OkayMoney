b3a63af573679f2f2e60ea718e2476d23f929597
Protocs <utki.nikita@yandex.ru>
2018-12-02 19:59:06 +0500

    Добавил атрибут Transaction.date, Transaction.note

--- okaymoney/transaction.py
+++ okaymoney/transaction.py
@@ -1,10 +1,12 @@
 class Transaction:
     """Доход или расход."""
 
-    def __init__(self, category, delta):
+    def __init__(self, category, delta, date, note):
         """
         :param category: категория транзакции.
         :param delta: изменение баланса. Положительное число - доход, отрицательное - расход.
         """
         self.category = category
         self.delta = delta
+        self.date = date
+        self.note = note
