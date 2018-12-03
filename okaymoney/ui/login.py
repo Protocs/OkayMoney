b6236b4e33bb536008cd0c322ad07c9b25de142c
makeitokay <andrey.mu11@yandex.ru>
2018-12-03 17:53:37 +0500

    Реализовано: 1) Отображение всех пользователей в окне LoginWindow

--- okaymoney/ui/login.py
+++ okaymoney/ui/login.py
@@ -1,8 +1,11 @@
 from .ui_window import UIWindow
 from .dialogs.user_registration import UserRegistrationDialog
 from okaymoney.user import User
-from okaymoney.user_save_load import save
+from okaymoney.user_save_load import save, load
 from .messagebox import error
+from .widgets.user_login_button import UserLoginButton
+import os
+
 
 class LoginWindow(UIWindow):
     """Класс окна входа в приложение.
@@ -15,7 +18,8 @@ class LoginWindow(UIWindow):
     def __init__(self):
         super().__init__()
         self.add_account.clicked.connect(self.show_add_user_dialog)
-
+        self.users = []
+        self.show_users()
 
     def show_add_user_dialog(self):
         self.add_user_dialog = UserRegistrationDialog()
@@ -38,13 +42,26 @@ class LoginWindow(UIWindow):
             repeated_password = self.add_user_dialog.repeat_password.text()
             # Создание аккаунта с паролем не реализовано.
         if name:
+            if name in [user for user in self.users]:
+                error('Пользователь с таким именем уже существует', self.add_user_dialog)
+                return
             acc = User(name, 0)
             save(acc)
             self.add_user_dialog.hide()
+            self.show_users()
         else:
             error('Введите имя пользователя', self.add_user_dialog)
 
     def close_dialog(self):
         self.add_user_dialog.hide()
 
-
+    def show_users(self):
+        for file in os.listdir():
+            if '.okm' in file:
+                user = load(file)
+                print(self.users, user.name)
+                if user.name not in self.users:
+                    button = UserLoginButton()
+                    button.name.setText(user.name)
+                    self.login_buttons_layout.addWidget(button)
+                    self.users.append(user.name)
