874d10c9abb23113fec66d8802fc7e5b86516a60
makeitokay <andrey.mu11@yandex.ru>
2018-12-05 21:20:08 +0500

    Добавлен класс диалогового окна, показывающего всех пользователей

---
+++ okaymoney/ui/dialogs/more_users.py
@@ -0,0 +1,12 @@
+from .ui_dialog import UIDialog
+
+
+class MoreUsersDialog(UIDialog):
+    """Диалог окна, показывающего всех пользователей.
+
+    *Файл интерфейса:* ``ui/dialogs/more_users_dialog.ui``
+    """
+
+    ui_path = 'ui/dialogs/more_users_dialog.ui'
+
+    ...
