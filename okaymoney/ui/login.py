19629332f6edc1dd74055abc3a9c1473db367cb8
makeitokay <andrey.mu11@yandex.ru>
2018-12-06 21:46:37 +0500

    Добавлена возможность добавить аватарку (отображение аватарки не реализовано, путь к аватарке лежит в переменной)

--- okaymoney/ui/login.py
+++ okaymoney/ui/login.py
@@ -5,6 +5,9 @@ from okaymoney.user import User
 from okaymoney.user_save_load import save, load
 from .messagebox import error
 from .widgets.user_login_button import UserLoginButton
+from PyQt5.QtWidgets import QFileDialog, QGraphicsScene
+from PyQt5.QtGui import QPixmap
+from PIL import Image
 import os
 
 
@@ -21,6 +24,7 @@ class LoginWindow(UIWindow):
         self.add_account.clicked.connect(self.show_add_user_dialog)
         self.show_more_users_btn.clicked.connect(self.show_more_users_dialog)
         self.users = [file[:file.find('.okm')] for file in os.listdir() if '.okm' in file]
+        self.avatar = None
         if len(self.users) <= 5:
             self.show_more_users_btn.hide()
         if len(self.users) > 0:
@@ -31,7 +35,22 @@ class LoginWindow(UIWindow):
         self.add_user_dialog = UserRegistrationDialog()
         self.add_user_dialog.ok_button.clicked.connect(self.add_user)
         self.add_user_dialog.cancel_button.clicked.connect(self.close_dialog)
+        self.add_user_dialog.add_avatar_btn.clicked.connect(self.add_avatar)
         self.add_user_dialog.exec()
+
+    def add_avatar(self):
+        filename = QFileDialog.getOpenFileName(self.add_user_dialog, 'Choose avatar')
+        try:
+            image = Image.open(filename[0])
+            if image.size[0] == 128 and image.size[1] == 128:
+                self.avatar = filename[0]
+                self.add_user_dialog.label_2.setText(filename[0].split('/')[-1])
+            else:
+                raise Exception
+        except Exception as e:
+            print(e)
+            error('Ошибка, попробуйте загрузить картинку снова.', self)
+
 
     def show_more_users_dialog(self):
         self.more_users_dialog = MoreUsersDialog()
