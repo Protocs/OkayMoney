7eecf92c62d8d7de8f703e2c8045723a040fc5bf
Protocs <utki.nikita@yandex.ru>
2018-12-06 22:52:01 +0500

    Убрал User.balance, добавил User.avatar_path

--- okaymoney/user.py
+++ okaymoney/user.py
@@ -1,20 +1,25 @@
-from decimal import Decimal
+import os
 
 
 class User:
     """Класс пользователя, который содержит всю информацию о пользователе."""
 
-    def __init__(self, name, balance):
+    def __init__(self, name, avatar_path):
         """
         :param name: имя пользователя.
-        :param balance: начальный баланс.
+        :param avatar_path: путь до аватарки.
         """
         self.name = name
-        self.balance = Decimal(balance)
-        self._accounts = []
+        self.accounts = []
+        self.avatar_path = avatar_path
 
         # Категории доходов и расходов
         self.income_categories = []
         self.spend_categories = []
 
         self.SAVE_PATH = name + '.okm'
+
+
+def get_user_names_in_current_dir():
+    """Возвращает имена всех созданных пользователей (те, что в текущей папке)."""
+    return [file.split('.')[0] for file in os.listdir('.') if file.endswith('.okm')]
