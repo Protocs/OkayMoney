a56db151d631efec2502cfbd6982483e60ad26c1
Protocs <utki.nikita@yandex.ru>
2018-12-06 18:49:32 +0500

    Добавил виджет PieChart

---
+++ okaymoney/ui/widgets/pie_chart.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+
+class PieChart(QWidget):
+    """Круговая диаграмма."""
+
+    def __init__(self):
+        super().__init__()
+
+        figure, self.axes = plt.subplots()
+        self.axes.axis('equal')  # Фиксируем пропорции, чтобы рисовался круг, а не овал
+
+        self.canvas = FigureCanvasQTAgg(figure)
+        layout = QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+    def update_chart(self, data):
+        """Создает или обновляет диаграмму."""
+        labels = [d[0] + '\n' + str(d[1]) + ' ₽' for d in data]
+        values = [d[1] for d in data]
+        explode = [0.01 for _ in range(len(data))]
+
+        self.axes.pie(values, labels=labels, explode=explode, startangle=90, labeldistance=0.5)
+        self.canvas.draw()
+
+
+if __name__ == '__main__':
+    # Пример использования
+
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    pc = PieChart()
+    plt.title('Test')
+    pc.update_chart([('Покупки', 100), ('Зарплата', 200)])
+    pc.show()
+    exit(app.exec())
