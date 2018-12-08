669c070d64197e7dd8ec69a9fdbc0636fc2389f4
Protocs <utki.nikita@yandex.ru>
2018-12-08 22:57:30 +0500

    Добавил фильтровку типов транзакций в PieChart

--- okaymoney/ui/widgets/pie_chart.py
+++ okaymoney/ui/widgets/pie_chart.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
 
+from ...util import INCOME, SPEND
 
 BACKGROUND_GRAY = str(55 / 255)
 
@@ -12,6 +13,11 @@ class PieChart:
     def __init__(self, widget):
         super().__init__()
 
+        self._transaction_type = None
+        self.transaction_type = INCOME
+
+        self.titles = {INCOME: 'Доходы', SPEND: 'Расходы'}
+
         figure, self.axes = plt.subplots()
         self.axes.axis('equal')  # Фиксируем пропорции, чтобы рисовался круг, а не овал
         figure.set_facecolor(BACKGROUND_GRAY)
@@ -20,6 +26,18 @@ class PieChart:
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
         widget.setLayout(layout)
+
+    @property
+    def transaction_type(self):
+        return self._transaction_type
+
+    @transaction_type.setter
+    def transaction_type(self, tp):
+        self._transaction_type = tp
+
+        title = plt.title(self.titles[self.transaction_type])
+        plt.setp(title, color='w')
+        self.update_chart([t.to_pie_data() for t in self.checked_accounts if t.type == self.transaction_type])
 
     def update_chart(self, data):
         """Создает или обновляет диаграмму."""
