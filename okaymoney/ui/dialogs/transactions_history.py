0799ff1ac63eac665704b3ef4a8edbeda32377aa
makeitokay <andrey.mu11@yandex.ru>
2018-12-10 16:16:43 +0500

    Кнопки "удалить" и "изменить" отключаются после каждого перезаполнения комбобокса с транзакциями

--- okaymoney/ui/dialogs/transactions_history.py
+++ okaymoney/ui/dialogs/transactions_history.py
@@ -34,14 +34,13 @@ class TransactionsHistoryDialog(UIDialog):
             [acc for acc in self.user.accounts if acc.name == self.accounts_box.currentText()][0]
         self.transactions = [acc.transactions for acc in self.user.accounts
                              if self.account.name == acc.name][0]
-        if not self.transactions:
-            self.change_btn.setEnabled(False)
-            self.delete_btn.setEnabled(False)
         self.history_transactions.clear()
         for transaction in self.transactions:
             self.history_transactions.addItem(
                 "{}{}\t{}".format('+' if transaction.type == INCOME else '',
                                   str(transaction.delta), transaction.date.toString()))
+        self.change_btn.setEnabled(False)
+        self.delete_btn.setEnabled(False)
 
     def show_details(self):
         self.change_btn.setEnabled(True)
