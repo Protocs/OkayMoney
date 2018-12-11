76fcc9d0ab13ade6216c32954b3521b258b27ebf
makeitokay <andrey.mu11@yandex.ru>
2018-12-11 20:22:38 +0500

    Добавил функцию, открывающую информационное окно (QMessageBox) с определенным сообщением.

--- okaymoney/ui/messagebox.py
+++ okaymoney/ui/messagebox.py
@@ -10,3 +10,11 @@ def error(msg, parent=None):
     box.setWindowTitle('Ошибка')
     box.setIcon(QMessageBox.Critical)
     box.exec()
+
+def information(msg, parent=None):
+    """Показывает окно с информацией ``msg`` над окном/виджетом ``parent``."""
+    box = QMessageBox(parent)
+    box.setText(msg)
+    box.setWindowTitle('Внимание!')
+    box.setIcon(QMessageBox.Information)
+    box.exec()
