4a73b6d834fb47a583b821caa8c6dab0ffa703ef
makeitokay <andrey.mu11@yandex.ru>
2018-12-14 15:30:38 +0500

    Добавление новых категорий реализовано в диалоговом окне.

---
+++ okaymoney/ui/dialogs/new_category.py
@@ -0,0 +1,40 @@
+from .ui_dialog import UIDialog
+from ..messagebox import error
+from ...user_save_load import save
+
+
+class NewCategoryDialog(UIDialog):
+    """Диалог добавления новой категории.
+
+    *Файл интерфейса:* ``ui/dialogs/new_category.ui``
+    """
+
+    ui_path = 'ui/dialogs/new_category.ui'
+
+    def __init__(self, categories_list, user):
+        super().__init__()
+
+        self.categories_list = categories_list
+        self.user = user
+        self.add_category_btn.clicked.connect(self.add_category)
+
+    def add_category(self):
+        name = self.category_name.text()
+        try:
+            if not name:
+                error("Введите название категории", self)
+                return
+            if name in self.categories_list:
+                error("Категория с таким названием уже существует", self)
+                return
+            if len(name) > 25:
+                error("Длина названия категории не должна превышать 25 символов", self)
+                return
+
+            self.categories_list.append(name)
+            save(self.user, self)
+
+            self.close()
+
+        except Exception:
+            error('Ошибка, попробуйте еще раз.', self)
--- okaymoney/ui/dialogs/settings.py
+++ okaymoney/ui/dialogs/settings.py
@@ -1,10 +1,10 @@
 from .ui_dialog import UIDialog
 from .user_registration import add_avatar
-from PyQt5.QtWidgets import QFileDialog, QListWidgetItem
+from PyQt5.QtWidgets import QFileDialog
 from ...user_save_load import remove, save
 from ..messagebox import error
 from .confirm import ConfirmActionDialog
-
+from .new_category import NewCategoryDialog
 
 class SettingsDialog(UIDialog):
     """Диалог настроек.
@@ -21,8 +21,6 @@ class SettingsDialog(UIDialog):
         self.login_window = login_window
         self.main_window = main_window
         self.avatar = self.user.avatar
-        self.spend_categories_list = self.user.spend_categories.copy()
-        self.income_categories_list = self.user.income_categories.copy()
 
         self.tabWidget.currentChanged.connect(self.change_window_size)
         self.tabWidget.setCurrentIndex(0)
@@ -35,9 +33,9 @@ class SettingsDialog(UIDialog):
 
         self.fill_income_categories()
         self.fill_spend_categories()
-        self.spend_categories.itemClicked.connect(
+        self.spend_categories.currentItemChanged.connect(
             lambda x: self.delete_spend_category_btn.setEnabled(True))
-        self.income_categories.itemClicked.connect(
+        self.income_categories.currentItemChanged.connect(
             lambda x: self.delete_income_category_btn.setEnabled(True))
         self.delete_spend_category_btn.clicked.connect(self.get_categories)
         self.delete_income_category_btn.clicked.connect(self.get_categories)
@@ -76,83 +74,45 @@ class SettingsDialog(UIDialog):
 
     def fill_spend_categories(self):
         self.spend_categories.clear()
-        self.spend_categories.addItems(self.spend_categories_list)
+        self.spend_categories.addItems(self.user.spend_categories)
         self.delete_spend_category_btn.setEnabled(False)
 
     def fill_income_categories(self):
         self.income_categories.clear()
-        self.income_categories.addItems(self.income_categories_list)
+        self.income_categories.addItems(self.user.income_categories)
         self.delete_income_category_btn.setEnabled(False)
 
     def get_categories(self):
         obj = self.sender().objectName()
         if obj == "add_spend_category_btn":
-            self.add_category(self.spend_categories, self.spend_categories_list,
-                              self.delete_spend_category_btn, self.fill_spend_categories)
-
+            self.add_category(self.user.spend_categories, self.fill_spend_categories)
         elif obj == "add_income_category_btn":
-            self.add_category(self.income_categories, self.income_categories_list,
-                              self.delete_income_category_btn, self.fill_income_categories)
+            self.add_category(self.user.income_categories, self.fill_income_categories)
         elif obj == "delete_spend_category_btn":
-            self.delete_category(self.spend_categories_list, self.spend_categories,
+            self.delete_category(self.user.spend_categories, self.spend_categories,
                                  self.fill_spend_categories)
         elif obj == "delete_income_category_btn":
-            self.delete_category(self.income_categories_list, self.income_categories,
+            self.delete_category(self.user.income_categories, self.income_categories,
                                  self.fill_income_categories)
 
-    def add_category(self, categories, categories_list, del_button, fill_function):
-        if categories_list:
-            last_item = categories.item(len(categories_list) - 1)
-            if categories.isPersistentEditorOpen(last_item):
-                if not self.check_empty_category(categories, categories_list, last_item):
-                    categories_list[-1] = last_item.text()
-                    fill_function()
-                else:
-                    return
-        item = QListWidgetItem()
+    def add_category(self, categories_list, fill_function):
         if len(categories_list) < 10:
-            categories.addItem(item)
-            categories.openPersistentEditor(item)
-            categories.setCurrentItem(item)
-            categories_list.append("")
-            del_button.setEnabled(True)
+            self.new_category_dialog = NewCategoryDialog(categories_list, self.user)
+            self.new_category_dialog.exec()
+            fill_function()
         else:
             error("Максимальное допустимое количество категорий - 10", self)
-
-    def check_empty_category(self, categories, categories_list, item):
-        categories.closePersistentEditor(item)
-        item = categories.item(len(categories_list) - 1)
-        if not item.text():
-            error("Нельзя добавлять пустые категории.", self)
-            categories.openPersistentEditor(item)
-            return True
-        return False
 
     def delete_category(self, categories_list, categories, fill_function):
         categories_list.pop(categories.currentRow())
         fill_function()
 
     def apply_changes(self):
-        last_spend_item = self.spend_categories.item(len(self.spend_categories_list) - 1)
-        last_income_item = self.income_categories.item(len(self.income_categories_list) - 1)
-        if self.check_empty_category(self.spend_categories,
-                                     self.spend_categories_list, last_spend_item) \
-                or self.check_empty_category(self.income_categories,
-                                             self.income_categories_list, last_income_item):
-            return
-
         remove(self.user, self)
 
         self.user.name = self.user_name.text()
         self.user.SAVE_PATH = self.user.name + '.okm'
         self.user.avatar = self.avatar
-
-        if self.spend_categories_list:
-            self.spend_categories_list[-1] = last_spend_item.text()
-        if self.income_categories_list:
-            self.income_categories_list[-1] = last_income_item.text()
-        self.user.income_categories = self.income_categories_list
-        self.user.spend_categories = self.spend_categories_list
 
         save(self.user, self)
 
