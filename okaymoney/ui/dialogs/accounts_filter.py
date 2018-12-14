e3184622fd55a54ca7b9db8a8058447ed5b095ba
makeitokay <andrey.mu11@yandex.ru>
2018-12-14 16:17:30 +0500

    Убрал тире при выводе счетов в окне фильтра счетов

--- okaymoney/ui/dialogs/accounts_filter.py
+++ okaymoney/ui/dialogs/accounts_filter.py
@@ -1,10 +1,10 @@
 from .ui_dialog import UIDialog
 from PyQt5.QtWidgets import QListWidgetItem, QCheckBox
 from ...user_save_load import save
-
+from PyQt5.Qt import QFont
 
 class AccountsFilterDialog(UIDialog):
-    """Диалог окна, показывающего всех пользователей.
+    """Диалог фильтра счетов.
 
     *Файл интерфейса:* ``ui/dialogs/accounts_filter_dialog.ui``
     """
@@ -21,14 +21,15 @@ class AccountsFilterDialog(UIDialog):
         for account in user.accounts:
             item = QListWidgetItem()
             self.accounts_list.addItem(item)
-            checkbox = QCheckBox(account.name + ' - ' + str(account.money) + ' ₽', self)
+            checkbox = QCheckBox(account.name + '\t\t' + str(account.money) + ' ₽', self)
+            checkbox.setFont(QFont("Gotham Pro Medium"))
             checkbox.setChecked(account.checked)
             self.checkboxes.append(checkbox)
             self.accounts_list.setItemWidget(item, checkbox)
 
     def choose_accounts(self):
         for account in self.user.accounts:
-            account.checked = account.name in [' - '.join(checkbox.text().split(' - ')[:-1])
+            account.checked = account.name in ['\t\t'.join(checkbox.text().split('\t\t')[:-1])
                                                for checkbox in self.checkboxes if checkbox.isChecked()]
         save(self.user, self)
         self.close()
