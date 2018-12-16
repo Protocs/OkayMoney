73b3fd8d0923ecdc666c8978ae04193714af0824
nikitakosatka <nikitakosatka@yandex.ru>
2018-12-16 20:09:23 +0500

    Добавил словарь THEMES с темами для диаграммы

--- okaymoney/util.py
+++ okaymoney/util.py
@@ -16,6 +16,12 @@ def shorten(text, max_length):
 
 INCOME = 'i'
 SPEND = 's'
+THEMES = {'standard': ['#EA005E', '#3498db', '#8e44ad', '#f39c12', '#16a085', '#2ecc71', '#2c3e50',
+                       '#f39c12', '#f9d9f4', '#00ffea'],
+          'light': ['#e90060', '#34edff', '#e1fb00', '#ff9767', '#00bff8', '#f9a0c4', '#f9a0c4',
+                    '#fdde26', '#fdde26', '#fdde26'],
+          'hard': ['#b9003c', '#1078f8', '#1078f8', '#ff3823', '#ff3823', '#d95b77', '#d95b77',
+                   '#d95b77', '#d95b77', '#d95b77']}
 
 
 def mix_dicts(dicts):
