78450d927ff3476f897785b43ebf41e29e920218
makeitokay <andrey.mu11@yandex.ru>
2019-05-23 21:52:05 +0500

    Выбор даты в истории транзакций

--- okaymoney/ui/dialogs/transactions_history.py
+++ okaymoney/ui/dialogs/transactions_history.py
@@ -1,5 +1,5 @@
 from .ui_dialog import UIDialog
-from ...util import INCOME
+from ...util import INCOME, MONTHS
 from .change_transaction import TransactionChangeDialog
 from ...user_save_load import save
 
@@ -18,29 +18,45 @@ class TransactionsHistoryDialog(UIDialog):
         self.user = user
         self.main_window = main_window
 
+        self.account = None
         self.accounts_box.addItems([account.name for account in self.user.accounts])
         self.accounts_box.currentIndexChanged.connect(self.change_transactions)
         self.history_transactions.itemClicked.connect(self.show_details)
+        self.transactions = []
 
         self.change_btn.setEnabled(False)
         self.delete_btn.setEnabled(False)
         self.change_btn.clicked.connect(self.show_transaction_change_dialog)
         self.delete_btn.clicked.connect(self.delete_transaction)
 
+        self.yearSelect.currentIndexChanged.connect(self.change_current_year)
+        self.monthSelect.currentIndexChanged.connect(self.change_current_month)
+        self.daySelect.currentIndexChanged.connect(self.change_current_day)
+        self.current_year = None
+        self.current_month = None
+        self.current_day = None
+
         if self.user.accounts:
             self.change_transactions()
 
     def change_transactions(self):
+        old_account_name = self.account.name if self.account else None
         self.account = [
             acc
             for acc in self.user.accounts
             if acc.name == self.accounts_box.currentText()
         ][0]
-        self.transactions = [
-            acc.transactions
-            for acc in self.user.accounts
-            if self.account.name == acc.name
-        ][0]
+        self.transactions = []
+        for tr in self.account.transactions:
+            date = tr.date.date()
+            if self.current_year is not None and date.year() != self.current_year:
+                continue
+            if self.current_month is not None and date.month() != self.current_month:
+                continue
+            if self.current_day is not None and date.day() != self.current_day:
+                continue
+            self.transactions.append(tr)
+
         self.history_transactions.clear()
         for transaction in self.transactions:
             self.history_transactions.addItem(
@@ -54,6 +70,10 @@ class TransactionsHistoryDialog(UIDialog):
         self.delete_btn.setEnabled(False)
 
         self.details.setText("")
+
+        # Меняем комбобоксы с выбором даты только в том случае, если была смена счёта
+        if self.accounts_box.currentText() != old_account_name:
+            self.fill_date_selectors()
 
     def show_details(self):
         self.change_btn.setEnabled(True)
@@ -69,7 +89,7 @@ class TransactionsHistoryDialog(UIDialog):
         transaction = self.transactions[self.history_transactions.currentRow()]
         # month, year = transaction.date.date().month(), transaction.date.date().year()
         if self.account.remove_transaction(
-            transaction, self.user.negative_balance_information
+                transaction, self.user.negative_balance_information
         ):
             self.change_transactions()
             save(self.user, self)
@@ -88,3 +108,42 @@ class TransactionsHistoryDialog(UIDialog):
         # self.main_window.pie_chart.upd(month, year)
         self.main_window._update_monthly()
         self.main_window.fill_accounts()
+
+    def fill_date_selectors(self):
+        for select in [self.yearSelect, self.monthSelect, self.daySelect]:
+            select.clear()
+            select.addItem("Все")
+
+        years, months, days = [], [], []
+        for tr in self.transactions:
+            date = tr.date.date()
+            year = date.year()
+            month = date.month()
+            day = date.day()
+            if year not in years:
+                years.append(year)
+                self.yearSelect.addItem(str(year))
+            if month not in months:
+                months.append(month)
+                self.monthSelect.addItem(MONTHS[month - 1])
+            if day not in days:
+                days.append(day)
+                self.daySelect.addItem(str(day))
+
+    def change_current_year(self):
+        current = self.yearSelect.currentText()
+        if current:
+            self.current_year = None if current == "Все" else int(current)
+            self.change_transactions()
+
+    def change_current_month(self):
+        current = self.monthSelect.currentText()
+        if current:
+            self.current_month = None if current == "Все" else MONTHS.index(current) + 1
+            self.change_transactions()
+
+    def change_current_day(self):
+        current = self.daySelect.currentText()
+        if current:
+            self.current_day = None if current == "Все" else int(current)
+            self.change_transactions()
